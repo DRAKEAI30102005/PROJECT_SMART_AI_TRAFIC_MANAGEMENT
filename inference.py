6b4651af1ac9b4071eb1a9a9ee5750870cf7ca4a
DRAKEAI30102005 <aritra30102005@gmail.com>
2026-04-12 20:08:45 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -73,7 +73,8 @@ def load_local_env(env_path: Path) -> None:
 load_local_env(ROOT / ".env")
 
 IMAGE_NAME = os.getenv("IMAGE_NAME")  # If you are using docker image
-API_KEY = os.getenv("HF_TOKEN") or os.getenv("API_KEY")
+API_KEY = os.getenv("API_KEY") or os.getenv("HF_TOKEN")
+
 
 API_BASE_URL = os.getenv("API_BASE_URL") or "https://router.huggingface.co/v1"
 MODEL_NAME = os.getenv("MODEL_NAME") or "Qwen/Qwen2.5-72B-Instruct"
